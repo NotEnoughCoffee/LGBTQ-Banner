687a01a0372741e9f6806bd769c6c21036fd3ba8
= <=>
2022-07-14 11:19:42 -0700

    Added a Test View - will delete later

---
+++ LGBTQ+ Banner/Views/ClipTestView.swift
@@ -0,0 +1,20 @@
+//
+//  ClipTestView.swift
+//  LGBTQ+ Banner
+//
+//  Created by Kevin Cooper on 7/14/22.
+//
+
+import SwiftUI
+
+struct ClipTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ClipTestView_Previews: PreviewProvider {
+    static var previews: some View {
+        ClipTestView()
+    }
+}
